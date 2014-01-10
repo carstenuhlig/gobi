711f5f485b3016263cfe3f432bf888a0c86ece55
Carsten Uhlig <carsten.uhlig@gmail.com>
2014-01-11 00:21:13 +0100

    BLASTSearch Finished

--- BLASTSearch/src/blastsearch/BLASTSearch.java
+++ BLASTSearch/src/blastsearch/BLASTSearch.java
@@ -5,12 +5,14 @@
  */
 package blastsearch;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -22,7 +24,8 @@ public class BLASTSearch {
 
     public static FileSystem FS = FileSystems.getDefault();
     static int[][] substitutionmatrix;
-    static char[] charmatrix;
+    static char[] alphabet;
+    static HashMap<Character, Integer> alphabet_hashmap;
     static char[] sequence;
     static String seq;
     static int wordlength;
@@ -31,6 +34,7 @@ public class BLASTSearch {
     static Path fastafile;
     static Path substitionfile;
     static LinkedList<String> substrings;
+    static LinkedList<String> generatedStrings;
 
     /**
      * @param args the command line arguments
@@ -49,10 +53,83 @@ public class BLASTSearch {
         substitionfile = FS.getPath(args[3]);
         outputfile = FS.getPath(args[4]);
 
+        alphabet_hashmap = new HashMap<>();
+
         importSubstitionMatrix();
         importFastaFile();
         makeSubstrings();
+        genWords(wordlength);
+        exportToFile();
+    }
 
+    public static List<String> genWords(int length) {
+        generatedStrings = new LinkedList<>();
+        int iterations = (int) Math.pow(alphabet.length, length);
+        int[] intWord = new int[length];
+        int[] subSequence = null;
+        for (int i = 0; i < iterations; i++) {
+            for (String seq : substrings) {
+                subSequence = getIntWordFromString(seq);
+                if (calcDistance(intWord, subSequence) > threshold) {
+                    generatedStrings.add(getStringFromIntWord(intWord));
+                }
+            }
+            nextIntWord(intWord, alphabet.length);
+        }
+        return generatedStrings;
+    }
+
+    public static int[] getIntWordFromString(String seq) {
+        int[] returnintarray = new int[seq.length()];
+        int counter = 0;
+        for (char c : seq.toCharArray()) {
+            try {
+                returnintarray[counter] = alphabet_hashmap.get(c);
+            } catch (NullPointerException e) {
+                returnintarray[counter] = alphabet.length-1;
+            }
+            counter++;
+        }
+        return returnintarray;
+    }
+
+    public static int calcDistance(int[] wordA, int[] wordB) {
+        int sum = 0;
+        for (int i = 0; i < wordA.length; i++) {
+            sum += substitutionmatrix[wordA[i]][wordB[i]];
+        }
+        return sum;
+    }
+
+    //TODO Testen
+    public static void nextIntWord(int[] currentWord, int alphabet_length) {
+        boolean inc = true;
+        int i = 0;
+        while (inc == true && wordlength > i) {
+            currentWord[i]++;
+            if (currentWord[i] == alphabet_length) {
+                currentWord[i] = 0;
+            } else {
+                inc = false;
+            }
+            i++;
+        }
+    }
+
+    public static char[] getCharFromIntWord(int[] intWord) {
+        char[] charWord = new char[intWord.length];
+        for (int i = 0; i < intWord.length; i++) {
+            charWord[i] = alphabet[intWord[i]];
+        }
+        return charWord;
+    }
+
+    public static String getStringFromIntWord(int[] intWord) {
+        StringBuilder sb = new StringBuilder();
+        for (int i : intWord) {
+            sb.append(alphabet[i]);
+        }
+        return sb.toString();
     }
 
     public static void importSubstitionMatrix() throws IOException {
@@ -66,12 +143,13 @@ public class BLASTSearch {
                     String[] bla = line.split("\\s+");
 
                     //matrizen initialisieren
-                    charmatrix = new char[bla.length];
+                    alphabet = new char[bla.length];
                     substitutionmatrix = new int[bla.length][bla.length];
                     //TODO überprüfung ob bei split mit leerzeichen vorher auch kein leeres feld in String array entsteht
 
-                    for (int i = 0; i < bla.length; i++) {
-                        charmatrix[i] = bla[i].charAt(0);
+                    for (int i = 1; i < bla.length; i++) {
+                        alphabet[i] = bla[i].charAt(0);
+                        alphabet_hashmap.put(bla[i].charAt(0), i);
                     }
                 } else {
                     String[] bla = line.split("\\s+");
@@ -97,8 +175,17 @@ public class BLASTSearch {
     }
 
     public static void makeSubstrings() {
+        substrings = new LinkedList<>();
         for (int i = 0; i < seq.length() - 1 - wordlength; i++) {
             substrings.add(seq.substring(i, i + wordlength));
         }
+    }
+
+    public static void exportToFile() throws IOException {
+        BufferedWriter bla = Files.newBufferedWriter(outputfile, StandardCharsets.UTF_8 );
+        for (String string : generatedStrings) {
+            bla.write(string + "\n");
+        }
+        bla.close();
     }
 }
