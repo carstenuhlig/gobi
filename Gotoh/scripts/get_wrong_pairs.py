0366fc9d0fc3dadd836240b5448c559abc80704b
Carsten Uhlig <carsten.uhlig@gmail.com>
2014-01-19 01:14:41 +0100

    Ant & test script udpate

---
+++ Gotoh/scripts/get_wrong_pairs.py
@@ -0,0 +1,6 @@
+__author__ = 'Carsten'
+
+reader = open(sys.argv[1], 'r')
+writer = open(sys.argv[2], 'w')
+
+def 
--- Gotoh/scripts/test_gotoh.sh
+++ Gotoh/scripts/test_gotoh.sh
@@ -3,8 +3,10 @@
 # Evaluierung 1
 # abgaben verzeichnis stand 19.12.13: /home/proj/biocluster/praktikum/genprakt-ws13/abgaben/assignment1
 
-GOBI_GOTOH_DIR=/home/proj/biocluster/praktikum/genprakt-ws13/abgaben/assignment1/
-BENCHMARK_DIR=/home/proj/biosoft/praktikum/genprakt-ws13/assignment1/
+# GOBI_GOTOH_DIR=/home/proj/biocluster/praktikum/genprakt-ws13/abgaben/assignment1/
+GOBI_GOTOH_DIR=./
+# BENCHMARK_DIR=/home/proj/biosoft/praktikum/genprakt-ws13/assignment1/
+BENCHMARK_DIR=../original_res/
 MODE=( freeshift global local )
 
 # for m in MODE; do
@@ -14,21 +16,25 @@ MODE=( freeshift global local )
 # AUSGABE=uhligc_$MODE.out
 
 for m in ${MODE[@]}; do
-	AUSGABE=uhligc_$m.out
-	java -Xms2G -Xmx4G -jar ${GOBI_GOTOH_DIR}uhligc/gotoh.jar -pairs $BENCHMARK_DIR/cathscop.inpairs -seqlib $BENCHMARK_DIR/domains.seqlib -mode $MODE -printali > $AUSGABE
+	AUSGABE=uhligc_${m}.out
+	echo Generating $AUSGABE
+	java -jar ${GOBI_GOTOH_DIR}Gotoh.jar -pairs ${BENCHMARK_DIR}sanity.pairs -seqlib ${BENCHMARK_DIR}domains.seqlib -mode $MODE -printali > $AUSGABE
 done
 
 #exit 0
 # noch keine richtige Implementierung von Matrix input
 
 # Evaluierung 2
-GOTOH_REFERENCE=/home/proj/biosoft/praktikum/genprakt-ws13/assignment1/gotoh
-DAYHOFF_MATRIX=/home/proj/biosoft/praktikum/genprakt-ws13/assignment1/matrices/dayhoff.mat
+GOTOH_REFERENCE=../original_res/gotoh
+DAYHOFF_MATRIX=../original_res/matrices/dayhoff.mat
+echo check now
 
 for m in ${MODE[@]}; do
-	AUSGABE=uhligc_$m.out
+	AUSGABE=uhligc_${m}.out
 	$GOTOH_REFERENCE -mode $MODE -check $AUSGABE –matrix $DAYHOFF_MATRIX > $AUSGABE.check 2>&1
 done
+
+echo finished
 
 # for name in ${names[@]}
 # do
