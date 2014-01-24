f453a1d0474a00c1bb7b6de183601437ac18bd88
Carsten Uhlig <carsten.uhlig@gmail.com>
2014-01-24 13:41:52 +0100

    Prosite Init

---
+++ GOAnnotate/scripts/converttofasta.py
@@ -0,0 +1,13 @@
+#! python
+
+import sys
+
+rawfile = open(sys.argv[1], 'r')
+processedfile = open(sys.argv[2], 'w')
+
+for line in rawfile:
+    name = line.split(':')[0]
+    seq = line.split(':')[1]
+    processedfile.write(">" + name + "\n" + seq + "\n")
+rawfile.close()
+processedfile.close()
